bf247fccb2e0887be24d15b1b66f04038873abf2
Scott Lassen <slassen49@gmail.com>
2019-07-18 11:16:28 -0400

    parse lines without comments

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lightenv
-Lightweight .env parser with no dependencies or config
+lightenv is a lightweight .env parser with no dependencies or config
+
+### Install and use
+
+1. Run `npm i -s lightenv` in your project
+2. `require('lightenv');` in a file such as `index.js` in the same directory as your `.env` file before you use any environment variables.
---
+++ index.js
@@ -0,0 +1,16 @@
+const lines = require('fs').readFileSync('./.env', 'utf-8').split('\n');
+
+lines.forEach((line) => {
+  // a line should have at least 3 characters (k=v)
+  if (line && line.charAt(0) != '#' && line.length > 2) {
+    const equality = line.indexOf('=');
+    if (equality !== -1) {
+      const key = line.substr(0, equality);
+      const value = line.substr(equality + 1, line.length);
+
+      if (!process.env[key]) {
+        process.env[key] = value;
+      }
+    }
+  }
+});
